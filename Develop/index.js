8c194886d6202c649e8cdd12e3308ba4f4febb91
Mona Lisa <naomiroznik@gmail.com>
2022-11-03 11:03:25 -0600

    starting the array of questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,24 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
+// Generate the README
+const generateMarkdown = require('./Develop/utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username',
+
+
+    }
+
+
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
