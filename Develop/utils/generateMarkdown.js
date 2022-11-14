33c8d861eb47298a28067fd8d8344790777d81d0
Mona Lisa <naomiroznik@gmail.com>
2022-11-14 14:24:43 -0500

    writing the markdown generation

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'none') {
+    return ''
+  }
+  return `![Project License Badge](https://img.shields.io/badge/license-${license}-brightgreen)`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'none') {
+    return `- [License](#license)`
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'none') {
+    return ''
+  }
+  return `
+  ## License
+  
+  Licensed under ${license}
+  `
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// function generateMarkdown(data) {
+//   return `# ${data.title}
 
-`;
+// `;
+// }
+
+// module.exports = generateMarkdown;
+
+generateMarkdown = (data) => {
+  const {username, email, title, description, installation, usage, contribution, test, license} = data
+
+  return `
+  # ${title}
+  ${renderLicenseBadge(license)}
+
+  ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributors](#contributors)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+    ## Description
+    ${description}
+
+    ## Installation
+    ${installation}
+
+    ## Usage
+    ${usage}
+
+    ## Contributors
+    ${contribution}
+
+    ## Tests
+    ${test}
+
+    ## Questions
+    [My GitHub](https://github.com${username})
+
+    I can be reached at ${email}
+
+    ${renderLicenseSection(license)}
+  `;
 }
 
 module.exports = generateMarkdown;
