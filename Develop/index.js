89a6728341d93c8105f2f5cb34c1a251169f4fb4
Mona Lisa <naomiroznik@gmail.com>
2022-11-14 11:25:50 -0500

    adding input questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,62 +4,109 @@ const fs = require('fs');
 // Generate the README
 const generateMarkdown = require('./Develop/utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'username',
-        message: 'Enter your GitHub username',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("Enter your GitHub username");
-            }
-            return true;
-        }
-    },
-    // github repo
-    {
-    type: 'input',
-    name: 'repository',
-    message: 'Enter the name of your GitHub repository',
-    // validate
-    validate: function(answer) {
-        if (answer.length < 1) {
-            return console.log("Enter the name of your GitHub repository");
-        }
-        return true;
-    }
-    },
-    // title
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Enter the title of your project',
-        // validate
-        validate: function(answer) {
-            if (answer.length < 1) {
-                return console.log("Enter the title of your project");
-            }
-            return true;
-        }
-    },
-    // project installation
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Explain how user installs for Installation Section',
-    },
-    // usage
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter your project instructions and examples for Usage Section',
-    },
-    
+// TODO: Create an array of questions for user input attempt #1
+// const questions = [
+//     {
+//         type: 'input',
+//         name: 'username',
+//         message: 'Enter your GitHub username',
+//         validate: function (answer) {
+//             if (answer.length < 1) {
+//                 return console.log("Enter your GitHub username");
+//             }
+//             return true;
+//         }
+//     },
+//     // github repo
+//     {
+//     type: 'input',
+//     name: 'repository',
+//     message: 'Enter the name of your GitHub repository',
+//     // validate
+//     validate: function(answer) {
+//         if (answer.length < 1) {
+//             return console.log("Enter the name of your GitHub repository");
+//         }
+//         return true;
+//     }
+//     },
+//     // title
+//     {
+//         type: 'input',
+//         name: 'description',
+//         message: 'Enter the title of your project',
+//         // validate
+//         validate: function(answer) {
+//             if (answer.length < 1) {
+//                 return console.log("Enter the title of your project");
+//             }
+//             return true;
+//         }
+//     },
+//     // project installation
+//     {
+//         type: 'input',
+//         name: 'installation',
+//         message: 'Explain how user installs for Installation Section',
+//     },
+//     // usage
+//     {
+//         type: 'input',
+//         name: 'usage',
+//         message: 'Enter your project instructions and examples for Usage Section',
+//     },
+// ];
 
-
-
-];
+// TODO: Create an array of questions for user input attempt #2
+const promptinfo = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter your GitHub username',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the project title',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a short description explaining the what, why, and how of your project.',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use. Include screenshots as needed.',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'List your collaborators, if any, with links to their GitHub profiles.',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).',
+        }
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
