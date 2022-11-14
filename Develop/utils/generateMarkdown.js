0698a979d925e5a7b2dc7ed40847b6c769d6c90e
Mona Lisa <naomiroznik@gmail.com>
2022-11-14 15:47:39 -0500

    finish the functions in generatemarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,80 +1,100 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license === 'none') {
-    return ''
+function renderLicenseBadge(data) {
+  let licenseType = data.license;
+  let licenseBadge = '';
+  if (licenseType === 'Apache') {
+    licenseBadge = `[![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`
   }
-  return `![Project License Badge](https://img.shields.io/badge/license-${license}-brightgreen)`
+  if (licenseType === 'GNU GPL') {
+    licenseBadge = `[![License: GNU GPL]https://img.shields.io/badge/License-GPLv3-blue.svg)]`
+  }
+  if (licenseType === 'MIT') {
+    licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+  }
+  else if (licenseType === '') {
+    licenseBadge = '';
+  }
+  return licenseBadge;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license !== 'none') {
-    return `- [License](#license)`
+function renderLicenseLink(data) {
+  let licenseType = data.license;
+  let licenseLink = '';
+  if (licenseType === 'Apache') {
+    licenseLink = `(https://opensource.org/licenses/Apache-2.0)`
   }
-  return ''
+  if (licenseType === 'GNU GPL') {
+    licenseLink = `(https://www.gnu.org/licenses/gpl-3.0)`
+  }
+  if (licenseType === 'MIT') {
+    licenseLink = `(https://opensource.org/licenses/MIT)`
+  }
+  else if (licenseType === '') {
+    licenseLink = '';
+  }
+  return licenseLink;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license === 'none') {
-    return ''
+function renderLicenseSection(data) {
+  const badge = renderLicenseBadge(data);
+  const link = renderLicenseLink(data);
+  console.log(badge);
+  console.log(link);
+  let licenseSection = '';
+  if (link !== '' && badge !== '') {
+    licenseSection = badge + 'This application is covered by ' + data.license + ' license ' + link;
+  return licenseSection
+  } 
+  else {
+    licenseSection = '';
+    return licenseSection;
   }
-  return `
-  ## License
-  
-  Licensed under ${license}
-  `
 }
 
 // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   return `# ${data.title}
-
-// `;
-// }
-
-// module.exports = generateMarkdown;
-
-generateMarkdown = (data) => {
-  const {username, email, title, description, installation, usage, contribution, test, license} = data
-
+function generateMarkdown(data) {
+ const licenseSection = renderLicenseSection(data);
+ const displayBadge = renderLicenseBadge(data);
   return `
-  # ${title}
-  ${renderLicenseBadge(license)}
+  # ${data.title}
+  ${displayBadge}
+
+  ## Description
+  ${data.description}
 
   ## Table of Contents
     - [Description](#description)
     - [Installation](#installation)
     - [Usage](#usage)
-    - [License](#license)
     - [Contributors](#contributors)
     - [Tests](#tests)
     - [Questions](#questions)
-
-    ## Description
-    ${description}
+    - [License](#license)
 
     ## Installation
-    ${installation}
+    ${data.installation}
 
     ## Usage
-    ${usage}
+    ${data.usage}
 
     ## Contributors
-    ${contribution}
+    ${data.contribution}
 
     ## Tests
-    ${test}
+    ${data.test}
+
+    ## License
+    ${licenseSection}
 
     ## Questions
     [My GitHub](https://github.com${username})
 
     I can be reached at ${email}
-
-    ${renderLicenseSection(license)}
   `;
 }
 
